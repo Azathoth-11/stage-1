aa92e7ce5516fc4dfeb8f3fdafaed2f9e32d74cb
Azathoth-11 <rohitsinha@gmail.com>
2024-07-02 21:52:34 +0530

    first commit

--- index.js
+++ index.js
@@ -12,15 +12,21 @@ app.get("/api/hello", async (req, res) => {
    if (userId) {
      userId = userId.replace(/"/g, '');
     }
-
+   console.log(userId);
     try{
-       const visitorIp = req.ip;
-       const response = await axios.get(`http://ip-api.com/json/${visitorIp}`);
+       // const visitorIp = req.ip;
+       const ip = await axios.get("https://api.ipify.org?format=json");
+       const ipAddr = ip.data.ip;
+       console.log(ipAddr);
+       const response = await axios.get(`http://ip-api.com/json/${ipAddr}`);
+      console.log(response)
        const city = response.data.city;
+       console.log(city);
 
        const weather = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${openWeatherMapApiKey}`)
        const weatherData = weather.data;
        const temp = weatherData.main.temp;
+       console.log(temp)
 
        res.json({
           "client_ip": `${req.ip}`,
